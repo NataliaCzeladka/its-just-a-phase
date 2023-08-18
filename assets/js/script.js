8dc1adc9dcf554fe0068f21098c717abdc538660
Natalia Czeladka <natalia.czeladka@gmail.com>
2023-08-18 01:50:20 +0100

    Add a switch statement to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,7 @@ document.getElementById("date").innerHTML = fullDate;
 
 const fetchMoonPhaseData = async () => {
     try {
-        const response = await fetch("https://moon-phase.p.rapidapi.com/basic", {
+        const response = await fetch("https://moon-phase.p.rapidapi.com/advanced", {
             "method": "GET",
             "headers": {
                 "x-rapidapi-host": "moon-phase.p.rapidapi.com",
@@ -38,7 +38,7 @@ const fetchMoonPhaseData = async () => {
         }
 
         const data = await response.json();
-        displayMoonPhase(data.phase_name);
+        displayMoonPhase(data.moon.phase_name);
     } catch (error) {
         console.error("Error fetching data:", error);
         displayMoonPhase("Error fetching moon phase data");
@@ -53,3 +53,39 @@ const displayMoonPhase = (text) => {
 fetchMoonPhaseData();
 
 
+// Matching the current Moon Phase with its picture and description
+const newPhase = "First Quarter";
+switch (newPhase) {
+    case "New Moon":
+        document.getElementById("moon-picture").innerHTML = "<img src='assets/images/new_moon.webp'>";
+        document.getElementById("moon-text").innerHTML = "The New Moon is the first lunar phase. It occurs when the Moon is positioned between the Earth and the Sun. The three objects are in approximate alignment. The entire illuminated part of the Moon is on the back side of the Moon, the half that we cannot see. At this phase, the lunar disk is not visible to the naked eye.";
+        break;
+    case "Waxing Crescent":
+        document.getElementById("moon-picture").innerHTML = "<img src='assets/images/waxing_crescent.webp'>";
+        document.getElementById("moon-text").innerHTML = "After the New Moon, the sunlit portion is increasing, but less than half, so it is a Waxing Crescent. 'Waxing' essentially means 'growing' or expanding in illumination, while 'crescent' refers to the phases where the moon is less than half illuminated.";
+        break;
+    case "First Quarter":
+        document.getElementById("moon-picture").innerHTML = "<img src='assets/images/first_quarter.webp' height='300'>";
+        document.getElementById("moon-text").innerHTML = "The First Quarter (also called a 'half moon'), happens when the Moon is at a 90 degree angle with respect to the Earth and the Sun. We are seeing exactly half of the Moon illuminated and half in shadow. We call it First Quarter, because the Moon has traveled about a quarter of the way around Earth since the New Moon.";
+        break;
+    case "Waxing Gibbous":
+        document.getElementById("moon-picture").innerHTML = "<img src='assets/images/waxing_gibbous.webp'>";
+        document.getElementById("moon-text").innerHTML = " The area of illumination continues to increase. More than half of the Moon's face appears to be getting sunlight. The word 'gibbous' refers to phases where the moon is more than half illuminated.";
+        break;
+    case "Full Moon":
+        document.getElementById("moon-picture").innerHTML = "<img src='assets/images/full_moon.webp'>";
+        document.getElementById("moon-text").innerHTML = "At a Full Moon, the Earth,  the Moon, and the Sun are in approximate alignment, just as the New Moon, but the Moon is on the opposite side of the Earth, so the entire sunlit part of the moon is facing us. The shadowed portion is entirely hidden from view.";
+        break;
+    case "Waning Gibbous":
+        document.getElementById("moon-picture").innerHTML = "<img src='assets/images/waning_gibbous.webp'>";
+        document.getElementById("moon-text").innerHTML = "More than half of the Moon's face appears to be getting sunlight, but the amount is decreasing. 'Waning' means 'shrinking' or decreasing in illumination, while 'gibbous' refers to phases where the Moon is more than half illuminated.";
+        break;
+    case "Third Gibbous":
+        document.getElementById("moon-picture").innerHTML = "<img src='assets/images/third_quarter.webp'>";
+        document.getElementById("moon-text").innerHTML = "The Third Quarter (also called 'Last Quarter' or a 'half moon'), happens when the Moon is at a 90 degree angle with respect to the Earth and the Sun. We are seeing exactly half of the Moon illuminated and half in shadow. The Moon has moved another quarter of the way around Earth, to the third quarter position.";
+        break;
+    case "Waning Crescent":
+        document.getElementById("moon-picture").innerHTML = "<img src='assets/images/waning_crescent.webp'>";
+        document.getElementById("moon-text").innerHTML = "Less than half of the Moon's face appears to be getting sunlight, and the amount is decreasing. 'Waning' means 'shrinking' or decreasing in illumination, while 'crescent' refers to the phases where the moon is less than half illuminated.";
+        break;
+}
