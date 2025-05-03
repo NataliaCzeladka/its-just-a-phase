d7fa4291bc59cb8ee83cdbb82f0f3cbb020b8c15
NataliaCzeladka <natalia.czeladka@gmail.com>
2025-05-03 22:27:32 +0000

    Fix slide stacking on page load

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,49 +1,50 @@
-//Credit: code for image carousel taken from LogRocket Frontend Analytics website and edited to fit the project's needs
-//blog.logrocket.com/build-image-carousel-from-scratch-vanilla-javascript/
+// Credit: code for image carousel taken from LogRocket Frontend Analytics website and edited to fit the project's needs
+// blog.logrocket.com/build-image-carousel-from-scratch-vanilla-javascript/
 
 // Select all slides
 const slides = document.querySelectorAll(".slide");
 
-// Loop through the slides and set each slides translateX property to index * 100%
+// Loop through the slides and set each slide's translateX property to index * 100%
 slides.forEach((slide, indx) => {
   slide.style.transform = `translateX(${indx * 100}%)`;
 });
 
-// select next slide button
+// Select next slide button
 const nextSlide = document.querySelector(".btn-next");
 
-// current slide counter
+// Current slide counter
 let curSlide = 0;
-// maximum number of slides
+// Maximum number of slides
 let maxSlide = slides.length - 1;
 
-// add event listener and navigation functionality
+// Add event listener and navigation functionality
 nextSlide.addEventListener("click", function () {
-  // check if current slide is the last and reset current slide
+  // Check if current slide is the last and prevent moving further
   if (curSlide === maxSlide) {
-    curSlide = maxSlide;
+    return; // Stops at the last slide
   } else {
     curSlide++;
   }
-  // move slide by -100%
+
+  // Move slide by -100%
   slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
 });
 
-// select prev slide button
+// Select prev slide button
 const prevSlide = document.querySelector(".btn-prev");
 
-// add event listener and navigation functionality
+// Add event listener and navigation functionality
 prevSlide.addEventListener("click", function () {
-  // check if current slide is the first and reset current slide to last
+  // Check if current slide is the first and prevent moving further
   if (curSlide === 0) {
-    curSlide = 0;
+    return; // Stops at the first slide
   } else {
     curSlide--;
   }
 
-  // move slide by 100%
+  // Move slide by 100%
   slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
